5c4a6167f2ec803a76dbf7cff1a222481b547a04
涂江林 <jianglin.tu@wimisoft.com>
2025-10-28 15:04:19 +0800

    feat: 1

--- src/input-number/basic.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { InputNumber } from 'antd-v';
-</script>
-
-<template>
-  <InputNumber :min="1" :max="10" :value="3" />
-</template>
--- src/input-number/controls.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons-vue';
-import { InputNumber } from 'antd-v';
-import { h } from 'vue';
-</script>
-
-<template>
-  <InputNumber :controls="{ upIcon: () => h(ArrowUpOutlined), downIcon: () => h(ArrowDownOutlined) }" />
-</template>
