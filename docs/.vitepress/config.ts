2be2573eef30537b2749b696815983522a23b1fd
涂江林 <jianglin.tu@wimisoft.com>
2025-11-03 17:43:15 +0800

    feat: github

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -146,7 +146,7 @@ export default defineConfig({
       },
     ],
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/tujianglin/antd-v-docs' }],
     search: { options: searchOptions() },
 
     footer: {
